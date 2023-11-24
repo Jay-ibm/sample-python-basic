944ce11e09e0becd13f9ff5770e9d78fff753a4f
jayc51 <jayant.kulkarni@ibm.com>
2023-11-24 06:53:55 -0800

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello New World!"
+
+
+@app.route("/other/")
+def other_hello():
+    return "Hello my other World!"
+
+
+if __name__ == "__main__":
+    port = os.environ.get("FLASK_PORT") or 8080
+    port = int(port)
+
+    app.run(port=port, host="0.0.0.0")
